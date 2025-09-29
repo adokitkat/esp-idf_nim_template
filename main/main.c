a35bed603ae05ce1d4a5d0e8dfba59e50316a4e5
Adam Múdry <mudry.ado@gmail.com>
2025-09-29 15:51:36 +0200

    fix: Remove irrelevant comment

--- main/main.c
+++ main/main.c
@@ -1,9 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
